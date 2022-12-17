aa0ea35576bfbcb750d2aa50021cba006e5588da
Polina <po11nakchik@gmail.com>
2022-12-17 18:21:43 +0400

    Заполнение формы регистрации

--- Auto/AuthorizationForm.cs
+++ Auto/AuthorizationForm.cs
@@ -15,6 +15,41 @@ namespace Auto
         public AuthorizationForm()
         {
             InitializeComponent();
+
+            this.passField.AutoSize = false;
+            this.passField.Size = new Size(this.passField.Size.Width, 40);
+
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void CloseButton_MouseEnter(object sender, EventArgs e)
+        {
+            CloseButton.ForeColor = Color.FromArgb(155, 82, 235);
+        }
+
+        private void CloseButton_MouseLeave(object sender, EventArgs e)
+        {
+            CloseButton.ForeColor = Color.White;
+        }
+
+        Point lastPoint;
+        private void AuthorizationForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+
+        }
+
+        private void AuthorizationForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
         }
     }
 }
--- Auto/Program.cs
+++ Auto/Program.cs
@@ -17,7 +17,7 @@ namespace Auto
             Console.WriteLine();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new AuthorizationForm());
         }
     }
 }
