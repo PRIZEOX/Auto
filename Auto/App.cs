ec748b38acbefb5815da4504d0e42e8c6443a668
Gorgio <stremousov.2016@mail.ru>
2022-12-19 21:57:47 +0400

    Реализация методов

--- Auto/App.cs
+++ Auto/App.cs
@@ -22,5 +22,50 @@ public class Class1
         //
         // TODO: Add constructor logic here
         //
+        //
+        // TODO: Add constructor logic here
+        //
+        //
+        // TODO: Add constructor logic here
+        //
+        //
+        // TODO: Add constructor logic here
+        //
+        //
+        // TODO: Add constructor logic here
+        //
+        //
+        // TODO: Add constructor logic here
+        //
+        //
+        // TODO: Add constructor logic here
+        //
+        //
+        // TODO: Add constructor logic here
+        //
+        //
+        // TODO: Add constructor logic here
+        //
+        //
+        // TODO: Add constructor logic here
+        //
+        //
+        // TODO: Add constructor logic here
+        //
+        //
+        // TODO: Add constructor logic here
+        //
+        //
+        // TODO: Add constructor logic here
+        //
+        //
+        // TODO: Add constructor logic here
+        //
+        //
+        // TODO: Add constructor logic here
+        //
+        //
+        // TODO: Add constructor logic here
+        //
     }
 }
