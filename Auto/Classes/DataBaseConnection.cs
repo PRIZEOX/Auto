56e14f0328a37c450709e9c175c3e6dcab2e4e8f
encayni <oganisyanararat1@gmail.com>
2022-12-19 20:26:11 +0400

    База данных

---
+++ Auto/Classes/DataBaseConnection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Auto
+{
+    internal class DataBaseConnection
+    {
+        SqlConnection connection = new SqlConnection(@"");
+    }
+}
